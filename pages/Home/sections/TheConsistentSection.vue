39f90cd8e63db6724ba3138503c0fc794cddff35
LeonidShv <matembit1996@gmail.com>
2024-03-01 23:02:44 +0200

    add style for consistent section, clean code

--- pages/Home/sections/TheConsistentSection.vue
+++ pages/Home/sections/TheConsistentSection.vue
@@ -1,14 +1,19 @@
 <template>
-  <section>
-    <h2>Consitent leads flow to streamline Your business growth.</h2>
-    <p>
-      We combine disruptive marketing techniques with proven tech solutions to
-      provide maximum business value.
-    </p>
-    <div>
+  <section class="consistent">
+    <h2 class="consistent__title font-s-small-title c--dark">
+      Consitent leads flow to streamline Your business growth.
+    </h2>
+    <div class="consistent__graphic-wrapper">
+      <p class="consistent__text font-s-base c--dark">
+        We combine disruptive marketing techniques with proven tech solutions to
+        provide maximum business value.
+      </p>
+
       <GraphicVertical :graphicVerticalItems="graphicVerticalItems" />
-      <GraphicHorizontal :graphicHorizontalItems="graphicHorizontalItems" />
     </div>
+  </section>
+  <section>
+    <GraphicHorizontal :graphicHorizontalItems="graphicHorizontalItems" />
   </section>
 </template>
 
@@ -18,60 +23,60 @@ import GraphicHorizontal from "@/pages/Home/components/GraphicHorizontal/Graphic
 
 const graphicVerticalItems = ref([
   {
-    rate: "123",
-    height: "0.9",
+    rate: "27",
+    height: "0.15",
   },
   {
-    rate: "523",
-    height: "0.5",
+    rate: "53",
+    height: "0.22",
   },
   {
-    rate: "823",
-    height: "0.8",
+    rate: "61",
+    height: "0.28",
   },
   {
-    rate: "1223",
+    rate: "30",
+    height: "0.21",
+  },
+  {
+    rate: "63",
+    height: "0.16",
+  },
+  {
+    rate: "55",
+    height: "0.3",
+  },
+  {
+    rate: "63",
     height: "0.4",
   },
   {
-    rate: "123",
-    height: "0.9",
-  },
-  {
-    rate: "523",
+    rate: "71",
     height: "0.5",
   },
   {
-    rate: "823",
+    rate: "76",
+    height: "0.42",
+  },
+  {
+    rate: "67",
+    height: "0.6",
+  },
+  {
+    rate: "88",
+    height: "0.7",
+  },
+  {
+    rate: "68",
     height: "0.8",
   },
   {
-    rate: "1223",
-    height: "0.4",
+    rate: "78",
+    height: "0.73",
   },
   {
-    rate: "123",
+    rate: "90",
     height: "0.9",
-  },
-  {
-    rate: "523",
-    height: "0.5",
-  },
-  {
-    rate: "823",
-    height: "0.8",
-  },
-  {
-    rate: "1223",
-    height: "0.4",
-  },
-  {
-    rate: "123",
-    height: "0.9",
-  },
-  {
-    rate: "523",
-    height: "0.5",
   },
 ]);
 
@@ -98,3 +103,27 @@ const graphicHorizontalItems = ref([
   },
 ]);
 </script>
+
+<style scoped lang="scss">
+.consistent {
+  height: 100vh;
+  padding: 20px 20px 0;
+
+  &__graphic-wrapper {
+    position: relative;
+    height: calc(100vh - 341px);
+  }
+
+  &__title {
+    max-width: 1275px;
+    margin: 107px 0 24px;
+  }
+
+  &__text {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 580px;
+  }
+}
+</style>
--- pages/Home/sections/TheGrowingSection.vue
+++ pages/Home/sections/TheGrowingSection.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="growing bg--red p-20 d-flex justify-s-b">
-    <h1 class="growing__title c--dark font-s-big-title">Growing businesses by building relationships</h1>
-    <p class="growing__text c--dark font-s-base">B2B Marketing & LinkedIn Lead Generation agency</p>
+    <h1 class="growing__title c--dark font-s-big-title">
+      Growing businesses by building relationships
+    </h1>
+    <p class="growing__text c--dark font-s-base">
+      B2B Marketing & LinkedIn Lead Generation agency
+    </p>
   </section>
 </template>
 
